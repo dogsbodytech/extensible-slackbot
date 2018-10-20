2842a080a5ca53d9a8bd52bc52b2aa5e96b8b5ad
J <jim@localhost>
2018-10-20 12:39:27 +0100

    hack lunch.py to use lunch.json reactions file when present

--- plugins/lunch.py
+++ plugins/lunch.py
@@ -1,2 +1,21 @@
+import json
+import random
+import os
+
 def msg_contains_react_lunch(message):
-    return 'pizza'
+    # Default reaction
+    reaction = 'pizza'
+    # Use the reactions file if it exists and contains data in the expected
+    # format
+    # The file is intended to be dynamic so we're just going to read it in again
+    # everytime this funciton is called
+    try:
+        # Look for a json file containing a list of reactions
+        with open(os.path.realpath(__file__).replace('.py', '.json')) as f:
+            reactions = json.load(f)
+
+        reaction = random.choice(reactions)
+    except:
+        pass
+
+    return reaction
